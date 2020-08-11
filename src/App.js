59ec9ef94e725f51c488df9e3b798b04e7885fb5
Bastien Gauthier <bastien_gau@hotmail.fr>
2020-08-11 21:17:34 +0200

    Modification du texte Learn React

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          J'apprend react
         </a>
       </header>
     </div>
